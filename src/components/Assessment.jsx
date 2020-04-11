5ecc0b2e78cba2060ffcb962c70bf6f45c3802a2
Xarri Jorge <kodinglife@gmail.com>
2020-04-11 22:29:36 +0000

    Added icon to assessment button

--- src/components/Assessment.jsx
+++ src/components/Assessment.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 export default () => (
   <div className="assessment">
-    <p>Click here for self-assessment</p>
-    <div className="assessmentBtn btn-floating fixed-action-btn"></div>
+    <h6>Click here for self-assessment</h6>
+    <div className="assessmentBtn btn-floating fixed-action-btn">
+      <a
+        href="https://forms.gle/NpDq8KXqnZnaJqXZ9"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="material-icons">description</i>
+      </a>
+    </div>
   </div>
 )
